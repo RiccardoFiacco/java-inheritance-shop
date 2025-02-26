eccbd9051c1b1a6c12188d1d1933cc294965095a
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-02-26 14:43:14 +0100

    done the last class

--- src/exercise/tv/Tv.java
+++ src/exercise/tv/Tv.java
@@ -1,5 +1,31 @@
 package exercise.tv;
+import exercise.product.Product;
 
-public class Tv {
+public class Tv extends Product {
+    private int dimension;
+    private boolean smart;
+
+    public Tv(String nome,String descrizione,float prezzo,int iva){
+        super(nome, descrizione, prezzo, iva);
+    }
+    public Tv(String nome,String descrizione,float prezzo,int iva,int dimension, boolean smart){
+        super(nome, descrizione, prezzo, iva);
+        this.dimension = dimension;
+        this.smart = smart;
+    }
+
+    public int getDimension(){
+        return this.dimension;
+    }
+    public boolean getSmart(){
+        return this.smart; 
+    };
+
+    public void setColor(int dimension){
+        this.dimension = dimension;
+    }
+    public void setSmart(boolean smart){
+        this.smart = smart;
+    }
 
 }
