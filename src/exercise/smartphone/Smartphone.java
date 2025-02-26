5c16ba6774a19294c9485d5232f0a5dd3b3f7825
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-02-26 14:39:29 +0100

    dene the class smartphone

--- src/exercise/smartphone/Smartphone.java
+++ src/exercise/smartphone/Smartphone.java
@@ -1,5 +1,30 @@
 package exercise.smartphone;
+import exercise.product.Product;
 
-public class Smartphone {
+public class Smartphone extends Product {
+    private int imei;
+    private int memory;
 
+    public Smartphone(String nome,String descrizione,float prezzo,int iva){
+        super(nome, descrizione, prezzo, iva);
+    }
+    public Smartphone(String nome,String descrizione,float prezzo,int iva, int imei, int memory){
+        super(nome, descrizione, prezzo, iva);
+        this.imei= imei;
+        this.memory = memory;
+    }
+
+    public int getImei(){
+        return this.imei;
+    };
+    public int getMemory(){
+        return this.memory;
+    };
+
+    public void setImei(int imei){
+        this.imei = imei;
+    }
+    public void setMemory(int memory){
+        this.memory = memory;
+    }
 }
